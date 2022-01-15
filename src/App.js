ab0f31fd61284b53939c154a1835c2e27b525af2
CoderDill <mddillon47@yahoo.com>
2022-01-15 13:32:27 -0800

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, NavBar} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <UserContext.Provider value={currUser}>
+          <div className="App">
+            <NavBar />
+            <main>
+              <Routes />
+            </main>
+          </div>
+        </UserContext.Provider>
+      </BrowserRouter>
   );
 }
 
