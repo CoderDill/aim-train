44ec8dd89a1ffe1ee2d932ce28c9b3914bdb559f
Matthew Dillon <mddillon47@gmail.com>
2022-01-27 17:32:20 -0800

    AimRange

--- src/AimRange.js
+++ src/AimRange.js
@@ -1,43 +1,40 @@
-import "./AimRange.css"
-import { useStopwatch } from 'react-timer-hook'
-import React, { useState } from "react"
+import "./AimRange.css";
+import { useStopwatch } from "react-timer-hook";
+import React, { useState } from "react";
 
 function AimRange() {
-  const { seconds, start, pause } = useStopwatch(0)
-  const [score, setScore] = useState(0)
-  const [targets, setTargets] = useState([])
+  const { seconds, start, pause } = useStopwatch(0);
+  const [score, setScore] = useState(0);
+  const [targets, setTargets] = useState([]);
 
-    function addScore() {
-        setScore(score + 1)
-    }
-  
-  
-    
-  function setActiveTarget() {
-    function makeTargets() {
-      for (let i = 0; i <= 2200; i++) {
-        targets.push(<div className="target" onClick={addScore}></div>);
-      }
-      return targets;
-    }
-    makeTargets()
-    setInterval(() => {
-      let random = [Math.floor(Math.random() * targets.length)];
-       return targets[random] = <div className="activeTarget" onClick={addScore}></div>
-      }, 1000)
-    }
-  
-  
-    return (
-      <div className="aimRange border">
-        <button className="btn btn-success" onClick={setActiveTarget}>
-          Start
-        </button>
-        <span style={{ padding: "1em" }}>Timer: {seconds}</span>
-        Score: {score}
-        <div className="targetList">{targets}</div>
-      </div>
-    );
+  function addScore() {
+    setScore(score + 1);
+  }
+
+
+  return (
+    <div className="aimRange border">
+      <button className="btn btn-success">
+        Start
+      </button>
+      <span style={{ padding: "1em" }}>Timer: {seconds}</span>
+      Score: {score}
+      
+        <div
+          style={{
+            position: "absolute",
+            top: "400px",
+            right: "120px",
+            height: "35px",
+            width: "35px",
+            borderRadius: "50%",
+            backgroundColor: "white",
+          }}
+          onClick={addScore}
+        ></div>
+      
+    </div>
+  );
 }
 
 export default AimRange;
