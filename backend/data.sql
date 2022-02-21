d643ffd3e924dbc933201013d63fee9e0f43b510
Matthew Dillon <mddillon47@gmail.com>
2022-02-20 16:38:37 -0800

    games table

--- backend/data.sql
+++ backend/data.sql
@@ -1,26 +1,26 @@
-DROP DATABASE IF EXISTS aim_train_db;
-
-CREATE DATABASE aim_train_db;
-
-\c aim_train_db;
-
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS rewards;
-
 CREATE TABLE "users" (
   "id" SERIAL PRIMARY KEY,
-  "username" text UNIQUE NOT NULL,
-  "password" text NOT NULL,
-  "email" text NOT NULL
+  "username" varchar,
+  "password" password,
+  "email" email,
+  "reward_level" int
 );
 
 CREATE TABLE "rewards" (
   "id" SERIAL PRIMARY KEY,
-  "name" text,
   "image" text,
   "user_id" int
 );
 
-ALTER TABLE "rewards" 
-ADD FOREIGN KEY ("user_id") 
-REFERENCES "users" ("id") ON DELETE CASCADE;
+CREATE TABLE "games" (
+  "id" SERIAL PRIMARY KEY,
+  "score" int,
+  "user_id" int,
+  "reward_id" int
+);
+
+ALTER TABLE "rewards" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "games" ADD FOREIGN KEY ("reward_id") REFERENCES "rewards" ("id");
+
+ALTER TABLE "games" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
