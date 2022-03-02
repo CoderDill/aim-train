2828e576d8c20270be9102adc64081880623abf6
Matthew Dillon <mddillon47@gmail.com>
2022-03-01 16:54:31 -0800

    Game routes/Model

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -3,7 +3,8 @@ import UserContext from "../auth/UserContext";
 
 export function Profile() {
   const { currUser }  = useContext(UserContext);
-  console.log(currUser)
+  
+  
   return (
     <>
       <h1>{currUser}'s Profile</h1>
